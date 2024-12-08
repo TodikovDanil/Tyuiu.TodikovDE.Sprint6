fcf7cd6611d8e4bf71869bf3b05bc93f9e3d7a00
TodikovDanil <todic.danil@gmail.com>
2024-12-08 16:44:42 +0300

    Task4

---
+++ Tyuiu.TodikovDE.Sprint6.Task4.V17.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.TodikovDE.Sprint6.Task4.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.TodikovDE.Sprint6.Task4.V17/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.TodikovDE.Sprint6.Task4.V17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
