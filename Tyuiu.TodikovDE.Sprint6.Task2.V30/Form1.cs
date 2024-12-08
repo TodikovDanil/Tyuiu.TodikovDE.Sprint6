ee4bee90be13d8f63966cda3bc8921176b533ed9
TodikovDanil <todic.danil@gmail.com>
2024-12-08 15:17:04 +0300

    Task2

---
+++ Tyuiu.TodikovDE.Sprint6.Task2.V30/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.TodikovDE.Sprint6.Task2.V30
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
