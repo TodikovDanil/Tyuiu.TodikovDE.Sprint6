9a74925a15835f3de7d029370412c2b4b8e98c67
TodikovDanil <todic.danil@gmail.com>
2024-12-08 14:44:38 +0300

    Task1

---
+++ Tyuiu.TodikovDE.Sprint6.Task1.V15.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.TodikovDE.Sprint6.Task1.V15.Lib
+{
+    public class DataService : ISprint6Task1V15
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double d = x - 0.7;
+                if (d == 0)
+                {
+                    y = 0;
+                    valueArray[count] = y;
+                    count++;
+                }
+                else
+                {
+                    y = Math.Round((Math.Cos(x)/x - 0.7) - Math.Sin(x) - 12 * x + 2, 2);
+                    valueArray[count] = y;
+                    count++;
+                }
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.TodikovDE.Sprint6.Task1.V15.Test/DataSrviceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.TodikovDE.Sprint6.Task1.V15.Test
+{
+    [TestClass]
+    public class DataSrviceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
