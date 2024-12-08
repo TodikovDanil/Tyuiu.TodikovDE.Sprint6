8863c4bfc0c4eda282c9bd5827f2efd0f5189ccc
TodikovDanil <todic.danil@gmail.com>
2024-12-08 15:35:10 +0300

    Task3

---
+++ Tyuiu.TodikovDE.Sprint6.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.TodikovDE.Sprint6.Task3.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
